f36d2d8ed12a6440d36728c1a6bdd51e26888de4
Trevor6546 <57647629+Trevor6546@users.noreply.github.com>
2023-10-06 14:23:25 -0700

    Add files via upload

---
+++ chap2.py
@@ -0,0 +1,18 @@
+#Data Types
+name  = "trevor Novak"
+print(name.title()) #makes first letter of variable uppercase
+print(name.upper()) #makes entire variable uppercase
+print(name.lower()) #makes entire variable lowercase
+first_name = "trevor"
+last_name = "novak"
+full_name = first_name + " " + last_name #string concatenation
+print(full_name.title()) 
+print("This is a \n NewLine") #\n creates a new line
+space = " blank spaces   "
+print(space.strip()) #removes blank spaces from a variable
+power = 3 ** 2 #is 3^2
+age = 18
+print("Happy " + str(age) + "th Birthday!") #str(age) converts age to a string value
+print(3/2) #1
+print(3/2.0) #1.5
+print(3.0/2) #1.5
